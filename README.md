b6837fd77f107fd147d674a3a405f777a91dc2c0
Keerthana <keerthanabhat22@gmail.com>
2025-06-30 21:02:38 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # IT-Helpdesk-Chatbot
+#how to run
+### step 1- create a conda environment after opening the repository
+```bash
+conda create -n chatbot python=3.10 -y
+
+```bash
+conda activate chatbot
+
+### step 2- install the requirements
+```bash
+pip install -r requirements.txt
+```
+
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+
+system_prompt= (
+    "You are an IT helpdesk assistant for a company." \
+    "Use the following pieces of retrieved context to answer the question." \
+    "If you do not know the answer, say 'I do not know the answer.'"
+    "Keep your answer concise and easy to understand."
+    "\n\n"
+    "{context}"
+)
